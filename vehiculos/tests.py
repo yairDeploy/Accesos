f22d740846695f2d673aaeff2f277af5c3e8d017
yair avila <vdyair2018@outlook.com>
2019-02-13 08:44:13 -0600

    Crea la seguna prueba de humo

--- vehiculos/tests.py
+++ vehiculos/tests.py
@@ -4,3 +4,6 @@ from django.test import TestCase
 class TestCatalogo(TestCase):
     def test_smoke(self):
         self.assertEquals(2,1+1)
+
+    def test_smoke_2(self):
+        self.assertFalse(False)
